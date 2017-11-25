182b701c6e8971ffc77d0a60dc582be93eee3fea
Tobias Almroth <almrooth@gmail.com>
2017-11-25 10:33:52 +0100

    mocha & nyc

---
+++ src/test/test.js
@@ -0,0 +1,13 @@
+/**
+ * Simple file to test
+ */
+"use strict";
+
+class Test {
+    test() {
+        return "This is my test";
+    }
+}
+
+// Export module
+module.exports = Test;
---
+++ test/test/test.js
@@ -0,0 +1,26 @@
+/* eslint-env mocha */
+
+/**
+ * File to test unittests
+ */
+
+"use strict";
+
+const assert = require('assert');
+const Test = require('../../src/test/test');
+
+describe("Check return value", function() {
+    it("Return type is string", function() {
+        let test = new Test();
+        let res = typeof test.test();
+
+        assert.equal(res, "string");
+    });
+
+    it("Return value is 'This is my test'", function() {
+        let test = new Test();
+        let res = test.test();
+
+        assert.equal(res, "This is my test");
+    });
+});
